938791211343ce526b2dbe8d7af98f419163bc80
Vijay jangid <vjjangid850@gmail.com>
2024-04-06 19:22:40 +0530

    new options added

--- nvim/lua/jv/core/options.lua
+++ nvim/lua/jv/core/options.lua
@@ -4,13 +4,25 @@ local opt = vim.opt
 opt.relativenumber = true
 opt.number = true
 opt.wrap = false
-opt.cursorline = true 
+opt.cursorline = true
 
-opt.shiftwidth = 2
-opt.tabstop = 2
+-- tabs and indentations
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.tabstop = 2    -- 2 spaces on tab press
 opt.expandtab = true
 opt.autoindent = true
 
+-- search settings
+opt.ignorecase = true -- ignore case when searching the word
+opt.smartcase = true  -- when use specific search with diff cases then turn off ignore case
+
+-- colors related
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
 opt.clipboard:append("unnamedplus")
 
 opt.swapfile = false
