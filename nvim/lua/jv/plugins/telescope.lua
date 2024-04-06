fafd04889e0fad1a997445c0a5e7485c7e00f143
Vijay jangid <vjjangid850@gmail.com>
2024-04-06 19:58:35 +0530

    telescope improvements

--- nvim/lua/jv/plugins/telescope.lua
+++ nvim/lua/jv/plugins/telescope.lua
@@ -1,58 +1,51 @@
 return {
-	'nvim-telescope/telescope.nvim',
-	tag = '0.1.5',
-	dependencies = {
-		'nvim-lua/plenary.nvim',
-		"nvim-telescope/telescope-live-grep-args.nvim",
-		version = "^1.0.0",
-	},
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.5',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-tree/nvim-web-devicons",
+  },
 
-	config = function()
-		local telescope = require("telescope")
-		local actions = require("telescope.actions")
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
 
-		telescope.load_extension("live_grep_args")
+    telescope.setup({
+      defaults = {
+        path_display = { "smart" },
+        mappings = {
+          i = {
+            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+            ["<C-j>"] = actions.move_selection_next,     -- move to next result
+            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+          },
+        },
+        layout_config = {
+          preview_width = 0.5
+        },
+        file_ignore_patterns = {
+          "bin",
+          "obj",
+          ".git/",
+          "node_module",
+        },
 
-		-- extensions = {
-		--
-		-- 	live_grep_args = {
-		-- 		layout_config = { width = 1 }
-		-- 	}
-		-- }
+      },
+    })
 
+    telescope.load_extension("fzf")
 
-		telescope.setup({
-			defaults = {
-				path_display = { "truncate " },
-				mappings = {
-					i = {
-						["<C-k>"] = actions.move_selection_previous, -- move to prev result
-						["<C-j>"] = actions.move_selection_next, -- move to next result
-						["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-					},
-				},
-				layout_config = {
-					preview_width = 0.5
-				},
-				file_ignore_patterns = {
-					"bin",
-					"obj",
-					".git/",
-					"node_module",
-				},
+    local keymap = vim.keymap
+    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+    keymap.set("n", "<leader>fw", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
 
-			},
-		})
-
-		local keymap = vim.keymap
-		keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-		keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-		keymap.set("n", "<leader>fw", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-		keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-		keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>", { desc = "git status" })
-		keymap.set("n", "<leader>gh", "<cmd>Telescope git_stash<cr>", { desc = "git stash" })
-		keymap.set("n", "<leader>gf", "<cmd>Telescope git_files<cr>", { desc = "git files" })
-		keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>", { desc = "git commits" })
-		keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>", { desc = "git branches" })
-	end,
+    keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>", { desc = "git status" })
+    keymap.set("n", "<leader>gh", "<cmd>Telescope git_stash<cr>", { desc = "git stash" })
+    keymap.set("n", "<leader>gf", "<cmd>Telescope git_files<cr>", { desc = "git files" })
+    keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>", { desc = "git commits" })
+    keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>", { desc = "git branches" })
+  end,
 }
